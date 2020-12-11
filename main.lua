2183cdd93509c4df08e699aaf2904e0d8ff20d2f
justinlua314 <blindsighterr3141@gmail.com>
2020-12-11 14:39:52 -0800

    Added simple examples of currently working patterns.

--- main.lua
+++ main.lua
@@ -1,14 +1,35 @@
 require("libraries")
 
-theDepth = 1
-
 function love.load()
+	pattern.new("spiral", {
+		colors = {pattern.randomColor(), pattern.randomColor()},
+		thickness = 20,
+		location = {x = 800, y = 300},
+		limit = 1600
+	})
+
+	pattern.new("line", {
+		colors = {
+			pattern.randomColor(),
+			pattern.randomColor(),
+			pattern.randomColor(),
+			pattern.randomColor(),
+			pattern.randomColor(),
+		},
+		thickness = 10,
+		lines = 10,
+		height = 200
+	})
+
+	pattern.render()
 end
 
 function love.update(dt)
 end
 
 function love.draw()
+	love.graphics.setColor(1, 1, 1)
+	love.graphics.draw(pattern.canvas, 0, 0)
 end
 
 function love.keypressed(key)
